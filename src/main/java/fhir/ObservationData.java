014c0f658e085eed7ad46ecc30f3deed2f7257fd
RenegadeWizard <ksychla5@gmail.com>
2020-06-23 10:38:23 +0200

    add more details

--- src/main/java/fhir/ObservationData.java
+++ src/main/java/fhir/ObservationData.java
@@ -3,56 +3,29 @@ package fhir;
 import javax.swing.*;
 import java.util.Date;
 
-public class ObservationData {
-    public String getId() {
-        return id;
-    }
-
-    public String getSourceId() {
-        return sourceId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
+public class ObservationData extends PatientDetailData {
 
     public String getCategory() {
         return category;
     }
 
-    public String getInfo() {
-        return info;
-    }
-
     public Double getValue() {
-        return value;
-    }
 
-    public Date getAdataTime() {
-        return adataTime;
+        return value;
     }
 
     public String getUnit() {
         return unit;
     }
 
-    private String id;
-    private String sourceId;
-    private String status;
     private String category;
-    private String info;
     private Double value;
-    private Date adataTime;
     private String unit;
 
     ObservationData(String id, String sourceId, String status, String category, String info, Double value, Date adataTime, String unit){
-        this.id=id;
-        this.sourceId=sourceId;
-        this.status=status;
-        this.category=category;
-        this.info=info;
-        this.value=value;
-        this.adataTime=adataTime;
-        this.unit=unit;
+        super(id, sourceId, status, info, adataTime, "Obserwacja");
+        this.category = category;
+        this.value = value;
+        this.unit = unit;
     }
 }
--- src/main/java/sample/Details.java
+++ src/main/java/sample/Details.java
@@ -1,34 +1,57 @@
 package sample;
 
+import fhir.MedicationData;
 import fhir.ObservationData;
-import fhir.PatientData;
+import fhir.PatientDetailData;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 import java.text.SimpleDateFormat;
 
 public class Details {
     private final Object prevController;
-    private final ObservationData observationData;
+    private final PatientDetailData patientDetailData;
     private final SimpleDateFormat mainFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     @FXML public BorderPane mainPane;
     @FXML public Label detailTitle;
     @FXML public Label detailType;
     @FXML public Label detailDate;
+    @FXML public VBox mainVBox;
 
 
-    public Details(Object prevController, ObservationData observationData) {
+    public Details(Object prevController, PatientDetailData patientDetailData) {
         this.prevController = prevController;
-        this.observationData = observationData;
+        this.patientDetailData = patientDetailData;
     }
 
     @FXML
     public void initialize(){
-        detailTitle.setText(observationData.getInfo());
-        detailType.setText("Obserwacja");
-        detailDate.setText(mainFormat.format(observationData.getAdataTime()));
+        detailTitle.setText(patientDetailData.getInfo());
+        detailType.setText(patientDetailData.getDescription());
+        detailDate.setText(mainFormat.format(patientDetailData.getAdataTime()));
+        if(patientDetailData.getClass() == ObservationData.class){
+            ObservationData data = (ObservationData) patientDetailData;
+            String result = String.format("%.2f", data.getValue());
+            createLabel("Wartość: "+result+" "+data.getUnit());
+            createLabel("Kategoria: "+data.getCategory());
+//            createLabel(data.getUnit());
+        } else if(patientDetailData.getClass() == MedicationData.class){
+            MedicationData data = (MedicationData) patientDetailData;
+            createLabel("Lekarz prowadzący: "+data.getMedicianName());
+        }
+    }
+
+    private void createLabel(String text){
+        Label label = new Label(text);
+        mainVBox.getChildren().add(label);
+    }
+
+    @FXML
+    public void editDetails(){
+
     }
 
     @FXML
